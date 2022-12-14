8e6beef4d6e38b59dd87623e82acfb629e9f442f
lacanella <107126483+lacanella@users.noreply.github.com>
2022-12-14 15:07:47 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Fine Weather React App</div>;
+  return (
+    <div className="App">
+      <h1>Fine Weather React App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://fantastic-raindrop-075015.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Karina Agakhanova
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/lacanella/fine-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
