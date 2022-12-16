1c3543f27bc69ef50f249de6c2bdf6914304868b
lacanella <107126483+lacanella@users.noreply.github.com>
2022-12-16 10:37:29 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Fine Weather React App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://fantastic-raindrop-075015.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Karina Agakhanova
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/lacanella/fine-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Fine Weather React App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://fantastic-raindrop-075015.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Karina Agakhanova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/lacanella/fine-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
